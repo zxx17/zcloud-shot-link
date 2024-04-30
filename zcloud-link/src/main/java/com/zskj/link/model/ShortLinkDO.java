3e492b89e8f21f8e2bfb33ecbf378e32bb884a0a
Xinxuan Zhuo <1575165952@qq.com>
2024-04-30 21:35:17 +0800

    sign: "0430b"

--- zcloud-link/src/main/java/com/zskj/link/model/ShortLinkDO.java
+++ zcloud-link/src/main/java/com/zskj/link/model/ShortLinkDO.java
@@ -23,7 +23,6 @@ public class ShortLinkDO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-      @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
